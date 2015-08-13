0729377f135cec36bfc5830267141acdf7c14a59
Samuel Atkins <sam@samatkins.co.uk>
2015-08-13 18:25:12 +0100

    Trying to implement a better raycasting routine, but currently it's pretty broken.

--- core/src/uk/co/samatkins/gbjam4/PlayScene.java
+++ core/src/uk/co/samatkins/gbjam4/PlayScene.java
@@ -19,9 +19,17 @@ public class PlayScene extends InputAdapter
 	private final Vector2 playerPosition = new Vector2();
 	private float playerFacingDirection;
 
-	private static final float rayStepLength = 0.1f;
-	private final Vector2 rayPosition = new Vector2();
-	private final Vector2 rayStep = new Vector2();
+	private static final float FOV = 60f;
+	private static final float cameraDistance = 0.5f;
+	private static final float cameraHalfWidth = (float)(cameraDistance * Math.tan(Math.toRadians(FOV) / 2.0));
+	private final Vector2 cameraPlane = new Vector2();
+	private final Vector2 cameraFacing = new Vector2();
+	private final Vector2 rayPos = new Vector2();
+	private final Vector2 rayDir = new Vector2();
+
+//	private static final float rayStepLength = 0.1f;
+//	private final Vector2 rayPosition = new Vector2();
+//	private final Vector2 rayStep = new Vector2();
 
 	public PlayScene(GBJam4 game) {
 		this.game = game;
@@ -68,32 +76,120 @@ public class PlayScene extends InputAdapter
 		shapeRenderer.rect(0, 0, GBJam4.SCREEN_WIDTH, GBJam4.SCREEN_HALF_HEIGHT);
 
 		// RAYS!
-		rayStep.set(MathUtils.cosDeg(playerFacingDirection), MathUtils.sinDeg(playerFacingDirection));
+		// http://lodev.org/cgtutor/raycasting.html
+		cameraPlane.set(
+			MathUtils.cosDeg(playerFacingDirection - 90) * cameraHalfWidth,
+			MathUtils.sinDeg(playerFacingDirection - 90) * cameraHalfWidth
+		);
+		cameraFacing.set(
+			MathUtils.cosDeg(playerFacingDirection) * cameraDistance,
+			MathUtils.sinDeg(playerFacingDirection) * cameraDistance
+		);
+		int dirX = (cameraFacing.x > 0) ? 1 : -1;
+		int dirY = (cameraFacing.y > 0) ? 1 : -1;
 
-		float offsetStepX = -rayStep.y * 0.01f;
-		float offsetStepY = rayStep.x * 0.01f;
+		for (int rayIndex=0; rayIndex < GBJam4.SCREEN_WIDTH; rayIndex++) {
+			float rayCameraX = 2 * rayIndex / GBJam4.SCREEN_HALF_WIDTH - 1;
+			rayPos.set(playerPosition);
+			rayDir.set(
+				dirX + cameraPlane.x * rayCameraX,
+				dirY + cameraPlane.y * rayCameraX
+			);
 
-		rayStep.scl(rayStepLength);
+			int mapX = (int)rayPos.x,
+				mapY = (int)rayPos.y;
 
-		for (int rayIndex = 0; rayIndex < GBJam4.SCREEN_WIDTH; rayIndex++) {
-			rayPosition.set(playerPosition);
-			float offset = (rayIndex - GBJam4.SCREEN_HALF_WIDTH);
-			rayPosition.add(offset * offsetStepX, offset * offsetStepY);
+			// Distance to next x or y side
+			float sideDistX, sideDistY;
 
-			float wallDistance = 0;
-			while (true) {
-				rayPosition.add(rayStep);
-				wallDistance += rayStepLength;
-				if (levelGeometry[(int) rayPosition.x][(int) rayPosition.y] > 0) {
-					// We hit something!
-					float wallHeight = MathUtils.round(GBJam4.SCREEN_HEIGHT / wallDistance * 0.5f) * 2;
-					shapeRenderer.setColor(GBJam4.Palette.Black);
-					shapeRenderer.rect(rayIndex, (GBJam4.SCREEN_HEIGHT - wallHeight) / 2, 1, wallHeight);
-//					Gdx.app.debug("Hit a wall!", "Distance: " + wallDistance + ", wall height: " + wallHeight);
-					break;
+			// Distance from one x or y side to the next x or y side
+			float deltaDistX = (float) Math.sqrt(1 + (rayDir.y * rayDir.y) / (rayDir.x * rayDir.x));
+			float deltaDistY = (float) Math.sqrt(1 + (rayDir.x * rayDir.x) / (rayDir.y * rayDir.y));
+			float perpWallDist;
+
+			int stepX, stepY;
+			boolean hit = false;
+			boolean sideHitIsY = false; // If true, wall faces N/S, else it faces E/W
+
+			//calculate step and initial sideDist
+			if (rayDir.x < 0) {
+				stepX = -1;
+				sideDistX = (rayPos.x - mapX) * deltaDistX;
+			} else {
+				stepX = 1;
+				sideDistX = (mapX + 1.0f - rayPos.x) * deltaDistX;
+			}
+			if (rayDir.y < 0) {
+				stepY = -1;
+				sideDistY = (rayPos.y - mapY) * deltaDistY;
+			} else {
+				stepY = 1;
+				sideDistY = (mapY + 1.0f - rayPos.y) * deltaDistY;
+			}
+
+			// DDA
+			while (!hit) {
+				if (sideDistX < sideDistY) {
+					sideDistX += deltaDistX;
+					mapX += stepX;
+					sideHitIsY = false;
+				} else {
+					sideDistY += deltaDistY;
+					mapY += stepY;
+					sideHitIsY = true;
+				}
+				// Did we hit?
+				if (levelGeometry[mapX][mapY] > 0) {
+					hit = true;
 				}
 			}
+
+			// Calculate distance to wall
+			if (!sideHitIsY) {
+				perpWallDist = Math.abs((mapX - rayPos.x + (1 - stepX) / 2) / rayDir.x);
+			} else {
+				perpWallDist = Math.abs((mapY - rayPos.y + (1 - stepY) / 2) / rayDir.y);
+			}
+
+			int lineHeight = Math.abs((int)(GBJam4.SCREEN_HEIGHT / perpWallDist));
+			lineHeight = MathUtils.clamp(lineHeight, 2, GBJam4.SCREEN_HEIGHT);
+
+			if (sideHitIsY) {
+				shapeRenderer.setColor(GBJam4.Palette.Dark);
+			} else {
+				shapeRenderer.setColor(GBJam4.Palette.Black);
+			}
+
+			shapeRenderer.rect(rayIndex, (GBJam4.SCREEN_HEIGHT - lineHeight) / 2, 1, lineHeight);
 		}
+
+//////////////////////////////////////
+//		rayStep.set(MathUtils.cosDeg(playerFacingDirection), MathUtils.sinDeg(playerFacingDirection));
+//
+//		float offsetStepX = -rayStep.y * 0.01f;
+//		float offsetStepY = rayStep.x * 0.01f;
+//
+//		rayStep.scl(rayStepLength);
+//
+//		for (int rayIndex = 0; rayIndex < GBJam4.SCREEN_WIDTH; rayIndex++) {
+//			rayPosition.set(playerPosition);
+//			float offset = (rayIndex - GBJam4.SCREEN_HALF_WIDTH);
+//			rayPosition.add(offset * offsetStepX, offset * offsetStepY);
+//
+//			float wallDistance = 0;
+//			while (true) {
+//				rayPosition.add(rayStep);
+//				wallDistance += rayStepLength;
+//				if (levelGeometry[(int) rayPosition.x][(int) rayPosition.y] > 0) {
+//					// We hit something!
+//					float wallHeight = MathUtils.round(GBJam4.SCREEN_HEIGHT / wallDistance * 0.5f) * 2;
+//					shapeRenderer.setColor(GBJam4.Palette.Black);
+//					shapeRenderer.rect(rayIndex, (GBJam4.SCREEN_HEIGHT - wallHeight) / 2, 1, wallHeight);
+////					Gdx.app.debug("Hit a wall!", "Distance: " + wallDistance + ", wall height: " + wallHeight);
+//					break;
+//				}
+//			}
+//		}
 
 		shapeRenderer.end();
 	}
